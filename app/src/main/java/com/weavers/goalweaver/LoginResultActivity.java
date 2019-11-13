26e0fa5195b7cb142aefafba842627a81097fadd
JihyoH <35987260+JihyoH@users.noreply.github.com>
2019-11-13 09:11:41 +0900

    Update LoginResultActivity.java
    WebView 관련 코드 추가

--- app/src/main/java/com/weavers/goalweaver/LoginResultActivity.java
+++ app/src/main/java/com/weavers/goalweaver/LoginResultActivity.java
@@ -1,48 +1,99 @@
-package com.weavers.goalweaver;
+package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.TextInputEditText;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
-import android.widget.RelativeLayout;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginResultActivity extends AppCompatActivity {
+    TextView textView_get;
+    Button longlong, nextpage;
+    private WebView mWebView;
 
-    TextView Textview_get;
-
-    //main(String[] args) 라고 생각하면 됨
+    // main(String[] args)라고 생각하면 됨.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-         setContentView(R.layout.activity_main);  // xml이랑 matching 해주는 이름 activity_main
+        setContentView(R.layout.activity_login);
 
-        Textview_get = findViewById(R.id.TextView_get);
+        //웹뷰 세팅
+        mWebView = findViewById(R.id.webView);//xml 자바코드 연결
+        mWebView.getSettings().setJavaScriptEnabled(true);//자바스크립트 허용
+        mWebView.loadUrl("https://rukaren.cafe24.com/");//웹뷰 실행
+        mWebView.setWebChromeClient(new WebChromeClient());//웹뷰에 크롬 사용 허용//이 부분이 없으면 크롬에서 alert가 뜨지 않음
+        mWebView.setWebViewClient(new WebViewClientClass());//새창열기 없이 웹뷰 내에서 다시 열기//페이지 이동 원활히 하기위해 사용
+
+
+
+        textView_get = findViewById(R.id.textView);
+
 
         Intent intent = getIntent();
 
-        Bundle bundle = intent.getExtras(); // bundle값을 반환해줌
-        String email = bundle.getString("email");
-        String password = bundle.getString("password");
+        Bundle bundle = intent.getExtras(); // bundle  값 반환
+        String id = bundle.getString("id");
+        String pw = bundle.getString("pw");
 
-        Textview_get.setText("환영합니다, "+email + "님 /  " + password);
-        /*try{
-            Thread.sleep(2500);
+        textView_get.setText("환영합니다. "+id+"님!! 비밀번호는 "+ pw+"입니다.");
 
-        }catch (InterruptedException e){
-            e.printStackTrace();
+        longlong = findViewById(R.id.longlong);
+        nextpage = findViewById(R.id.nextpage);
+
+
+        longlong.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Log.d("LONG_CLICK", "OnLongClickListener");
+                String msg = "Hello!";
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+                return true; // 다음 이벤트 계속 진행 false, 이벤트 완료 true
+            }
+        });
+
+
+        nextpage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("CLICK", "OnClickListener");
+
+                Intent intent_next = new Intent(LoginResultActivity.this, CanvasActivity.class);
+                intent_next.putExtra("msg", "next button clicked!");
+                startActivity(intent_next);
+
+
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {//뒤로가기 버튼 이벤트
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {//웹뷰에서 뒤로가기 버튼을 누르면 뒤로가짐
+            mWebView.goBack();
+            return true;
         }
-        startActivity(new Intent(this,MainActivity.class));
-        finish();*/
+        return super.onKeyDown(keyCode, event);
     }
 
-    public void onClick(View view){
-        Log.d("intent","여기까지는 성공1");
-        Intent intent = new Intent(this, NewsActivity.class);
-        Log.d("intent","여기까지는 성공2");
-        startActivity(intent);
+    private class WebViewClientClass extends WebViewClient {//페이지 이동
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            Log.d("check URL",url);
+            view.loadUrl(url);
+            return true;
+        }
     }
+
+
+
 
 }
